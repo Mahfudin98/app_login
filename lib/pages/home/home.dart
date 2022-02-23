c532d1e4f1551de5a0dc0518e914cc8fdc976a74
Mahfudin98 <mahfudinnawawi@gmail.com>
2022-02-23 08:40:16 +0700

    berhasil view data teams

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import '../../api/network.dart';
 
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
-  final _url = Uri.parse('http://app-r.lsskincare.id/api/teams?page=1');
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
-  Future<List<dynamic>> _teamsData() async {
-    final result = await http.get(_url, headers: Network().setHeaders());
-    return json.decode(result.body)['data'];
+  Future<List<dynamic>> _fecthDataUsers() async {
+    var token;
+    final SharedPreferences localStorage = await _prefs;
+    token = localStorage.getString('data')?.replaceAll("\"", "");
+
+    // print(token);
+    var headers = {'Authorization': 'Bearer $token'};
+    var request = http.MultipartRequest(
+        'GET', Uri.parse('https://app-r.lsskincare.id/api/teams?page=1'));
+
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+    var jsonData = jsonDecode(await response.stream.bytesToString());
+    return jsonData['data'];
   }
 
   @override
@@ -24,25 +37,24 @@ class Home extends StatelessWidget {
       ),
       body: Container(
         child: FutureBuilder<List<dynamic>>(
-          future: _teamsData(),
+          future: _fecthDataUsers(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
             if (snapshot.hasData) {
               return ListView.builder(
-                padding: EdgeInsets.all(10),
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage(
-                          'https://app-r.lsskincare.id/storage/teams/' +
-                              snapshot.data[index]['image']),
-                    ),
-                    title: Text(snapshot.data[index]['name']),
-                    subtitle: Text(snapshot.data[index]['email']),
-                  );
-                },
-              );
+                  padding: EdgeInsets.all(10),
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            "https://app-r.lsskincare.id/storage/teams/" +
+                                snapshot.data[index]['image']),
+                      ),
+                      title: Text(snapshot.data[index]['name']),
+                      subtitle: Text(snapshot.data[index]['email']),
+                    );
+                  });
             } else {
               return Center(child: CircularProgressIndicator());
             }
